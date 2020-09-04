31212cb04761f6e5533439a87e02fc0d35ff296c
Bhavik Joshi <bhavik@getwalnut.com>
2020-09-04 12:38:28 +0530

    Added Redux Form to create a new stream

--- client/src/Components/StreamComponents/StreamNew.js
+++ client/src/Components/StreamComponents/StreamNew.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamNew = () => {
-  return (
-    <div>
-      StreamNew
-    </div>
-  )
+class StreamNew extends React.Component{
+
+  renderErrors = ({ error, touched }) => {
+    if ( touched && error ) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      )
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input}/>
+        {this.renderErrors(meta)}
+      </div>
+    )
+  }
+
+  onSubmit = formValues => {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form className='ui form error' onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <Field name='title' component={this.renderInput} label='Enter Title' />
+        <Field name='description' component={this.renderInput} label='Enter Description' />
+        <button className="ui button primary">Submit</button>
+      </form>
+    )
+  }
 }
 
-export default StreamNew;
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.title) errors.title = 'You must enter a Title';
+  
+  if (!formValues.description) errors.description = 'You must enter a Description';
+  
+  return errors;
+}
+
+export default reduxForm({
+  form: 'createStream',
+  validate
+})(StreamNew);
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import App from './Components/App';
 import reducers from './reducers';
 
-const state = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const state = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDom.render(
   <Provider store={state}>
